c88fe37b887c2c8d24cfc391aa9a59138ee04c9e
Ben Wallace <beenwii@yahoo.com>
2020-02-18 08:30:01 -0700

    scope and lifetime example

---
+++ VariableScopeLifeTime/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+    Dim aName As String = "Pete" 'startup value
+    'aName = "Chucky"
+
+    Sub Main()
+        Dim aNumber As Integer
+        Dim anotherName As String
+        aNumber = 5
+        aName = "Chucky"
+        anotherName = "Jason"
+        'Console.WriteLine(aNumber)
+        Console.WriteLine(aName)
+        Console.WriteLine(anotherName)
+        MySub()
+        'Console.WriteLine(aNumber)
+        Console.WriteLine(aName)
+        Console.WriteLine(anotherName)
+        Console.ReadLine()
+    End Sub
+
+    Sub MySub()
+        Dim aNumber As Integer
+        Dim anotherName As String
+        aNumber = 3
+        aName = "Billy"
+        anotherName = "Freddie"
+    End Sub
+
+End Module
